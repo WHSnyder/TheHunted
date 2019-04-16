76031cddf36a9e6128de8759a04818f956e8372d
William Herald Snyder <will.herald.snyder@gmail.com>
2019-04-16 14:02:13 -0400

    Skippy animating correctly, key room, pump room first drafts

---
+++ Assets/Scripts/AudioTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float soundIncr = 0;
+    private AudioSource source;
+
+    void Start()
+    {
+        source = this.GetComponent<AudioSource>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        soundIncr += Time.deltaTime;
+
+        if (soundIncr > 1)
+        {
+            soundIncr = 0;
+            source.Play();
+        }
+    }
+}
