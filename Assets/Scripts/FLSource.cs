485e97883ae45c9b78a8c189e141b8f8a6cbb0da
William Herald Snyder <will@wireless-10-104-141-68.umd.edu>
2019-04-23 13:19:53 -0400

    long tunnel added

--- Assets/Scripts/FLSource.cs
+++ Assets/Scripts/FLSource.cs
@@ -3,58 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class FLSource : MonoBehaviour
-{
+public class FLSource : MonoBehaviour {
 
-
-    private Light source;
+    private Light source, bounce;
     private bool on = false;
 
     int layerMask;
 
+    Vector3 shot, reflection;
+    RaycastHit hit;
+
 
     // Start is called before the first frame update
-    void Start()
-    {
-
+    void Start(){
         layerMask = 1 << 8;
         layerMask = ~layerMask;
 
-
-
         source  = transform.GetChild(0).gameObject.GetComponent<Light>();
+        bounce = transform.GetChild(1).gameObject.GetComponent<Light>();
 
-
-
+        bounce.enabled = false;
+        source.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-        if (Input.GetMouseButtonDown(0))
-        {
+
+    // Update is called once per frame
+    void Update(){
+
+        if (Input.GetMouseButtonDown(0)){
             source.enabled = true;
             on = true;
+
+
         }
 
-        if (Input.GetMouseButtonUp(0))
-        {
+        if (Input.GetMouseButtonUp(0)){
             source.enabled = false;
+            bounce.enabled = false;
             on = false;
         }
 
-        if (on)
-        {
-            RaycastHit hit;
+        if (on){
             // Does the ray intersect any objects excluding the player layer
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity,layerMask))
-            {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                Debug.Log("Did Hit");
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity,layerMask)){
+
+                print("Shot");
+
+                Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+                Debug.DrawRay(transform.position, forward, Color.green);
+
+                shot = hit.point - this.transform.position;
+                reflection = Vector3.Reflect(shot, hit.normal);
+
+                bounce.transform.SetPositionAndRotation(hit.point, Quaternion.LookRotation(reflection));
+                bounce.enabled = true;
+
+                Debug.DrawRay(hit.point, reflection*10, Color.red);
             }
-
         }
-
     }
 }
