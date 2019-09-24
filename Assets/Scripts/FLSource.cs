69ae5538ff6a448be9e81616fcd6494a52b9d885
William Herald Snyder <will.herald.snyder@gmail.com>
2019-09-24 19:42:18 -0400

    Bouncing works, needs effect at hit point to distract from the jarring look of two discreet light beams

--- Assets/Scripts/FLSource.cs
+++ Assets/Scripts/FLSource.cs
@@ -4,13 +4,15 @@
 public class FLSource : MonoBehaviour{
 
     public Light source;
-    public GameObject beam,bounce;
+    private GameObject beam,bounce;
 
     private bool on;
     private float power = 200.0f;
 
-    Vector3 shot, reflection;
+    Vector3 shot, refl_forward,refl_up;
+    Transform objhit;
     RaycastHit hit;
+    private Material beam_material,bounce_material;
 
 
 
@@ -18,7 +20,10 @@ public class FLSource : MonoBehaviour{
     void Start(){
 
         beam = GameObject.Find("light_cone");
+        beam_material = beam.GetComponent<Renderer>().material;
+
         bounce = GameObject.Find("light_cone_bounce");
+        bounce_material = bounce.GetComponent<Renderer>().material;
 
         source = GameObject.Find("light").GetComponent<Light>(); 
         source.enabled = false;
@@ -35,15 +40,36 @@ public class FLSource : MonoBehaviour{
 
         if (Input.GetKeyDown(KeyCode.F)){
             source.enabled = !source.enabled;
-            //bounce.enabled = !bounce.enabled;
             on = !on;
             beam.SetActive(on);
             source.intensity = 0;
 
-            if (on){
-                source.intensity = 10;
-            }
+            bounce.SetActive(on);
         }
+
+
+        if (on){
+            source.intensity = 10;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 40, 1 << 11)){
+
+                objhit = hit.transform;
+
+                bounce.SetActive(true);
+                bounce.transform.position = hit.point;
+
+                refl_forward = Vector3.Reflect(transform.forward, hit.normal);
+                refl_up = Vector3.Reflect(transform.up, hit.normal);
+
+                bounce.transform.forward = refl_forward;
+                bounce.transform.right = Vector3.Cross(refl_up, refl_forward);
+                bounce.transform.up = -1.0f * refl_up;
+
+                bounce_material.color = objhit.gameObject.GetComponent<Renderer>().material.color * beam_material.color;
+            }
+            else bounce.SetActive(false);
+        }
+
 
         if (power <= 0.0f){
             source.enabled = false;
@@ -53,7 +79,7 @@ public class FLSource : MonoBehaviour{
         }
 
         if (on && (power > 0.0f)){
-            power -= 0.05f;
+            //power -= 0.05f;
         } 
     }
 }
