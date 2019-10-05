e151e343af35e6f8236e38d1cf0ae7c0224fef15
Will Snyder <will.herald.snyder@gmail.com>
2019-10-05 15:54:41 -0400

    Post effects working well enough, though will have to complicate build system...

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -29,6 +29,7 @@ public class Player : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start(){
+        
         Cursor.lockState = CursorLockMode.Locked;
 
         control = GetComponent<CharacterController>();
